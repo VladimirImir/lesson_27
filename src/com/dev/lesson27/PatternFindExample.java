133623fddce93c7ec61d19ec2106ee5810c2212e
VladimirImir <mimimvvv@gmail.com>
2023-06-16 23:17:12 +0300

    27.4 - Замена подстрок.

--- src/com/dev/lesson27/PatternFindExample.java
+++ src/com/dev/lesson27/PatternFindExample.java
@@ -7,13 +7,23 @@ public class PatternFindExample {
 
     public static void main(String[] args) {
         String phoneNumber = "das +375 (33) 898-33-33 dsasd +375 (29) 555-12-13 dafdvcx +375 (44) 898-12-22 weq";
-        String regex = "(?:\\+375)? ?\\((?<code>\\d{2})\\) ?\\d{3}-\\d{2}-\\1";
+        String regex = "(?:\\+375)? ?\\((?<code>\\d{2})\\) ?(\\d{3})-(\\d{2})-(\\d{2})";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(phoneNumber);
+        StringBuilder stringBuilder = new StringBuilder();
         while (matcher.find()){
-            System.out.println(matcher.group());
+            String gr1 = matcher.group(2);
+            String gr2 = matcher.group(3);
+            String gr3 = matcher.group(4);
+            matcher.appendReplacement(stringBuilder, "$2 - $3 - $4");
+
+            /*System.out.println(matcher.group());
             System.out.println(matcher.group(0));
-            System.out.println(matcher.group("code"));
+            System.out.println(matcher.group("code"));*/
         }
+        matcher.appendTail(stringBuilder);
+
+        System.out.println(stringBuilder);
+        System.out.println(phoneNumber.replaceAll(regex, "$2 - $3 - $4 | "));
     }
 }
